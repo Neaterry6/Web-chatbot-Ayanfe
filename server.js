89afa2042a9dcb54b641de1c1fc93568d5f54c0c
AYANFE BABY DEV ✍️❣️ <neaterry6@gmail.com>
2025-03-10 18:32:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "Public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "Public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/chat", async (req, res) => {
